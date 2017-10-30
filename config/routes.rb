4112c4956bcc180c0a04b6ba3c46c97fdcfa1527
Mariusz Kapcia <m.kapcia@nomtek.com>
2017-10-30 23:16:54 +0100

    Add dummy project create action and acceptance test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post '/projects', to: 'projects#create'
   post '/assignments', to: 'assignments#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
