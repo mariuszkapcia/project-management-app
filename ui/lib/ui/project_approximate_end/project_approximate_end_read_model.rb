d4e9c64771c3cc8771a9fcbcb5f077dc444a9e3f
Mariusz Kapcia <m.kapcia@nomtek.com>
2018-12-14 22:11:48 +0100

    Add project_registered domain event to project_approximate_end read model

--- ui/lib/ui/project_approximate_end/project_approximate_end_read_model.rb
+++ ui/lib/ui/project_approximate_end/project_approximate_end_read_model.rb
@@ -2,6 +2,8 @@ module UI
   class ProjectApproximateEndReadModel
     def call(event)
       case event
+        when ProjectManagement::ProjectRegistered
+          add_project(event.data[:uuid])
         when ProjectManagement::ProjectEstimated
           assign_estimation(event.data[:uuid], event.data[:hours])
           calculate_aproximate_end
@@ -22,20 +24,24 @@ module UI
 
     private
 
+    def add_project(project_uuid)
+      UI::ProjectApproximateEnd::Project.create!(uuid: project_uuid)
+    end
+
     def assign_estimation(project_uuid, estimation)
-      project            = UI::ProjectApproximateEnd::Project.where(uuid: project_uuid).first_or_create!
+      project            = UI::ProjectApproximateEnd::Project.find_by(uuid: project_uuid)
       project.estimation = estimation
       project.save!
     end
 
     def assign_deadline(project_uuid, deadline)
-      project          = UI::ProjectApproximateEnd::Project.where(uuid: project_uuid).first_or_create!
+      project          = UI::ProjectApproximateEnd::Project.find_by(uuid: project_uuid)
       project.deadline = deadline
       project.save!
     end
 
     def assign_developer_working_hours(project_uuid, developer_uuid, hours_per_week)
-      project                 = UI::ProjectApproximateEnd::Project.where(uuid: project_uuid).first_or_create!
+      project                 = UI::ProjectApproximateEnd::Project.find_by(uuid: project_uuid)
       project.working_hours ||= {}
       project.working_hours[developer_uuid] = hours_per_week
       project.save!
