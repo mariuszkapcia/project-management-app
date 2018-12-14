c0accc61a2847cceb53b51ca13ccfede93cfea56
Mariusz Kapcia <m.kapcia@nomtek.com>
2018-12-14 18:33:07 +0100

    Add error_handler service

---
+++ app/services/error_handler.rb
@@ -0,0 +1,20 @@
+class ErrorHandler
+  def self.json_for(error_code)
+    error_code = error_code.to_sym
+
+    ERRORS
+      .fetch(error_code)
+      .merge(message: message_for(error_code))
+  end
+
+  GENERAL = {
+    internal_server_error:          { code: '1000' },
+    incorrect_uuid:                 { code: '1001' }
+  }.freeze
+
+  ERRORS = GENERAL.freeze
+
+  def self.message_for(error_code)
+    I18n.t(error_code.to_sym, scope: :error_handler)
+  end
+end
