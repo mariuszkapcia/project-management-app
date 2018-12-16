d07d4d2c2929de9be19bf1bd0d4b6a7d8477ba5c
Mariusz Kapcia <m.kapcia@nomtek.com>
2018-12-16 21:55:59 +0100

    Use cents in amount value object

--- accounting/lib/accounting/value_objects/amount.rb
+++ accounting/lib/accounting/value_objects/amount.rb
@@ -2,47 +2,47 @@ module Accounting
   class Amount
     include Comparable
 
-    def initialize(value, currency)
-      @value    = value
+    def initialize(cents, currency)
+      @cents    = cents
       @currency = currency
     end
 
     def <=>(other)
-      self.class == other.class && value == other.value && currency == other.currency ? 0 : -1
+      self.class == other.class && cents == other.cents && currency == other.currency ? 0 : -1
     end
 
     def negative?
-      @value < 0
+      @cents < 0
     end
 
     def to_f
-      value
+      (cents / 100.to_f)
     end
 
     def to_s
-      "#{sprintf('%.2f', value)} #{currency}"
+      "#{sprintf('%.2f', (cents / 100.to_f))} #{currency}"
     end
 
     def +(other)
       raise ArgumentError if currency != other.currency
 
-      self.class.new(value + other.value, currency)
+      self.class.new(cents + other.cents, currency)
     end
 
     def -(other)
       raise ArgumentError if currency != other.currency
 
-      self.class.new(value - other.value, currency)
+      self.class.new(cents - other.cents, currency)
     end
 
     def *(other_value)
       raise ArgumentError unless other_value.class.in?([Integer, Float])
 
-      self.class.new(value * other_value, currency)
+      self.class.new(cents * other_value, currency)
     end
 
     protected
 
-    attr_reader :value, :currency
+    attr_reader :cents, :currency
   end
 end
--- accounting/spec/value_objects/amount_spec.rb
+++ accounting/spec/value_objects/amount_spec.rb
@@ -1,22 +1,24 @@
+require_dependency 'accounting'
+
 module Accounting
   RSpec.describe 'Amount value object' do
-    it { expect(Amount.new(12.50, 'PLN').to_f).to                 eq(12.50) }
-    it { expect(Amount.new(-2.50, 'GBP').to_f).to                 eq(-2.50) }
+    it { expect(Amount.new(1250, 'PLN').to_f).to                    eq(12.50) }
+    it { expect(Amount.new(-250, 'GBP').to_f).to                    eq(-2.50) }
 
-    it { expect(Amount.new(24, 'GBP')).to                         eq Amount.new(24, 'GBP') }
-    it { expect(Amount.new(0, 'EUR')).not_to                      eq Amount.new(0, 'PLN') }
+    it { expect(Amount.new(2400, 'GBP')).to                         eq Amount.new(2400, 'GBP') }
+    it { expect(Amount.new(0, 'EUR')).not_to                        eq Amount.new(0, 'PLN') }
 
-    it { expect(Amount.new(3, 'PLN') + Amount.new(2, 'PLN')).to   eq Amount.new(5, 'PLN') }
-    it { expect(Amount.new(3, 'PLN') - Amount.new(2, 'PLN')).to   eq Amount.new(1, 'PLN') }
+    it { expect(Amount.new(300, 'PLN') + Amount.new(200, 'PLN')).to eq Amount.new(500, 'PLN') }
+    it { expect(Amount.new(300, 'PLN') - Amount.new(200, 'PLN')).to eq Amount.new(100, 'PLN') }
 
-    it { expect{Amount.new(3, 'PLN') + Amount.new(2, 'EUR')}.to   raise_error(ArgumentError) }
-    it { expect{Amount.new(3, 'PLN') - Amount.new(2, 'EUR')}.to   raise_error(ArgumentError) }
+    it { expect{Amount.new(300, 'PLN') + Amount.new(200, 'EUR')}.to raise_error(ArgumentError) }
+    it { expect{Amount.new(300, 'PLN') - Amount.new(200, 'EUR')}.to raise_error(ArgumentError) }
 
-    it { expect(Amount.new(3, 'PLN') * 2.0).to                    eq Amount.new(6, 'PLN') }
+    it { expect(Amount.new(300, 'PLN') * 2.0).to                    eq Amount.new(600, 'PLN') }
 
-    it { expect(Amount.new(15.5, 'EUR').to_s).to                  eq('15.50 EUR') }
+    it { expect(Amount.new(1550, 'EUR').to_s).to                    eq('15.50 EUR') }
 
-    it { expect(Amount.new(-2.50, 'GBP').negative?).to            eq(true) }
-    it { expect(Amount.new(2.50, 'GBP').negative?).to             eq(false) }
+    it { expect(Amount.new(-250, 'GBP').negative?).to               eq(true) }
+    it { expect(Amount.new(250, 'GBP').negative?).to                eq(false) }
   end
 end
