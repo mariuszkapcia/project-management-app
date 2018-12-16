3d2015244f80f688263f13c7b42ecb9ef07aca57
Mariusz Kapcia <m.kapcia@nomtek.com>
2018-12-16 23:11:40 +0100

    Add list of orders to web ui

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   def index
     respond_to do |format|
       format.json { render json: UI::OrderListReadModel.new.all, status: :ok }
+      format.html { render action: :index, locals: { orders: UI::OrderListReadModel.new.all } }
     end
   end
 end
