f9e0a1d44aa94d63ea702fc69bb9da1a9b9c2cf4
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-04 19:31:28 +0100

    Fix incorrect reference to event store in project_kickoff_mailer domain service

--- notifications/lib/notifications/domain_services/project_kickoff_mailer.rb
+++ notifications/lib/notifications/domain_services/project_kickoff_mailer.rb
@@ -2,7 +2,7 @@ module Notifications
   class ProjectKickoffMailer
     # NOTE: I don't want to send a real email here so I publish domain event and display information on the UI.
     def deliver(project_uuid, project_name)
-      event_store.publish(
+      @event_store.publish(
         Notifications::ProjectKickoffEmailSent.strict(data: {
           project_uuid: project_uuid,
           project_name: project_name
