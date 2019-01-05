2efbcf3b095fdf08f1b58449f64a31ac3e8fc3d3
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-05 22:31:27 +0100

    Use project_list read model in project_kicked_off handler

--- notifications/lib/notifications/handlers/project_kicked_off_handler.rb
+++ notifications/lib/notifications/handlers/project_kicked_off_handler.rb
@@ -1,15 +1,18 @@
 module Notifications
   class ProjectKickedOffHandler
     def call(event)
+      project = @project_list_read_model.retrieve.find(event.data[:project_uuid])
+
       Notifications::ProjectKickoffMailer
-        .new(event_store: event_store)
-        .deliver(event.data[:project_uuid], 'project_name_placeholder')
+        .new(event_store: @event_store)
+        .deliver(event.data[:project_uuid], project[:name])
     end
 
     private
 
-    def event_store
-      Rails.configuration.event_store
+    def initialize(event_store:)
+      @event_store             = event_store
+      @project_list_read_model = ProjectList::Retriever.new(event_store: event_store)
     end
   end
 end
