dd62f6c373bbe722976ac722454abb45a048f9ca
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-16 23:15:41 +0100

    Load lib directory

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ProjectManagementApp
   class Application < Rails::Application
     config.load_defaults 5.1
 
+    config.paths.add 'lib/',                   eager_load: true
     config.paths.add 'command/lib',            eager_load: true
     config.paths.add 'project_management/lib', eager_load: true
     config.paths.add 'notifications/lib',      eager_load: true
